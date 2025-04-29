7c7886577bc78b33598e2463981ca76d86db221b
P2842317 <P2842317@GWHXX354WIN014W>
2025-04-29 13:29:41 +0100

    f

---
+++ Testing3/tstCheckout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCheckout
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCheckout AnCheckout  = new clsCheckout ();
+            //test to see that it exists
+            Assert.IsNotNull(AnCheckout);
+
+
+        }
+    }
+}
