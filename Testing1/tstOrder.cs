2f3e8725ee072f3287feebb1a731dd5c4399b705
Serghei <P2804092@my365.dmu.ac.uk>
2025-05-01 13:28:18 +0100

    Added testing for Order class

---
+++ Testing1/tstOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //Create and instance of the class we want to create
+            clsOrder anOrder = new clsOrder();
+            //Test to see that it exists
+            Assert.IsNotNull(anOrder);
+        }
+    }
+}
