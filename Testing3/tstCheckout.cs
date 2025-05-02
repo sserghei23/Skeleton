b100c6ea99c2e5607071d745b5fe86bb2c226d83
P2842317 <P2842317@GWHXX354WIN014W>
2025-05-02 12:27:54 +0100

    f

--- Testing3/tstCheckout.cs
+++ Testing3/tstCheckout.cs
@@ -15,6 +15,85 @@ namespace Testing3
             Assert.IsNotNull(AnCheckout);
 
 
+        }   
+
+        [TestMethod]
+        public void TotalCartValuePropertyOK()
+        {
+            clsCheckout AnCheckout = new clsCheckout();
+            //Create test data to assign to the proprty
+            string TestData = """500""";
+            //Assign Data to the proprty
+            AnCheckout.TotalCartValue = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AnCheckout.TotalCartValue, TestData);
+
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsCheckout AnCheckout = new clsCheckout();
+            //Create test data to assign to the proprty
+            DateTime TestData = DateTime.Now.Date();
+            //Assign Data to the proprty
+            AnCheckout.DateAdded = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AnCheckout.DateAdded, TestData);
+
+        }
+
+        [TestMethod]
+        public void CheckedOutPropertyOK()
+        {
+            clsCheckout AnCheckout = new clsCheckout();
+            //Create test data to assign to the proprty
+            Boolean TestData = true;
+            //Assign Data to the proprty
+            AnCheckout.CheckedOut = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AnCheckout.CheckedOut, TestData);
+
+        }
+
+        [TestMethod]
+        public void WatchidPropertyOK()
+        {
+            clsCheckout AnCheckout = new clsCheckout();
+            //Create test data to assign to the proprty
+            Int32 TestData = 1;
+            //Assign Data to the proprty
+            AnCheckout.Watchid = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AnCheckout.Watchid, TestData);
+
+        }
+
+        [TestMethod]
+        public void CustomeridPropertyOK()
+        {
+            clsCheckout AnCheckout = new clsCheckout();
+            //Create test data to assign to the proprty
+            Int32 TestData = 1;
+            //Assign Data to the proprty
+            AnCheckout.Customerid = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AnCheckout.Customerid, TestData);
+
+        }
+
+
+        [TestMethod]
+        public void CartidPropertyOK()
+        {
+            clsCheckout AnCheckout = new clsCheckout();
+            //Create test data to assign to the proprty
+            Int32 TestData = 1;
+            //Assign Data to the proprty
+            AnCheckout.Cartid = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AnCheckout.Cartid, TestData);
+
         }
     }
 }
