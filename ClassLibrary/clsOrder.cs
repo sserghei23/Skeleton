ca2b05aca899aa5a00dff4a1797a57d011addb77
Serghei <P2804092@my365.dmu.ac.uk>
2025-05-06 00:29:58 +0100

    Created Properties

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int StaffId { get; set; }
+        public string Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool Active { get; set; }
     }
 }
