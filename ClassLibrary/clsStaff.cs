22434b20be2cc69614077fcf52e030b27bd0f891
twoeyedmikewazokski <lordsofblox@gmail.com>
2025-05-06 10:55:14 +0100

    Added test methods for atributes in StaffManagement.

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public int StaffID { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsWorking { get; set; }
+        public int OrderID { get; set; }
+    }
+}
